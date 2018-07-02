1092393475fb1aa3bc23b4abefdcccb432011e59
miguelDiazFernandez <migueldiazfernandez0@gmail.com>
2018-07-02 16:40:51 +0200

    servicios con comentarios para javadoc

--- src/servicios/Servicios.java
+++ src/servicios/Servicios.java
@@ -4,9 +4,26 @@ import java.util.LinkedList;
 
 import modelos.ListaPeliculas;
 import modelos.Pelicula;
+/**
 
+* Esta clase es la capa servicios del proyecto
+
+* @author: Miguel Diaz Fernandez
+
+* @version: 02/07/2018/A
+
+* 
+
+*/
 public class Servicios implements IServicios {
+	//Campos de la capa servicios
+	 /**
 
+     * Constructor para la capa
+
+     * @param listaPelis La variable para listar las peliculas
+
+     */
 	private ListaPeliculas listaPelis ;
 
 	public Servicios(){
@@ -16,24 +33,48 @@ public class Servicios implements IServicios {
 		super();
 		this.listaPelis = listaPelis;
 	}
+	/**
 
+     * Constructor para la capa
+
+     * @param añadirPelicula metodo para añadir las peliculas
+
+     */
 	@Override
 	public void añadirPelicula(Object pelicula, String titulo, String director, int fechaEstreno, String categoria, String resumen, String url) {
 		listaPelis.añadirPelicula(pelicula, titulo, director, fechaEstreno, categoria, resumen, url);
 
+		/**
 
+	     * Constructor para la capa
+
+	     * @param modificarPelicula metodo para modificar las peliculas
+
+	     */
 	}
 	@Override
 	public void modificarPelicula(Object pelicula, String titulo, String director, int fechaEstreno, String categoria, String resumen) {
 		listaPelis.modificarPelicula(pelicula, titulo, director, fechaEstreno, categoria, resumen);
 		
 	}
+	/**
 
+     * Constructor para la capa
+
+     * @param buscarPelicula metodo para buscar las peliculas
+
+     */
 	public Object buscarPelicula(String titulo) {
 		Pelicula pelicula =(Pelicula) listaPelis.buscarPelicula(titulo);
 		return pelicula;
 	}
+	/**
 
+    * Constructor para la capa
+
+    * @param borrarPelicula metodo para borrar las peliculas
+
+    */
 	@Override
 	public void borrarPelicula(Object pelicula) {
 		listaPelis.borrarPelicula(pelicula);
@@ -43,16 +84,28 @@ public class Servicios implements IServicios {
 		}
 		
 	}
+	/**
 
+	    * Constructor para la capa
+
+	    * @param recorrerLista metodo para recorrer la Lista
+
+	    */
 	@Override
 	public void recorrerLista(Object lista) {
 		// TODO Auto-generated method stub
 		
 	}
-	
+	 /**
+
+     * Constructor para la capa
+
+     * @param listaPelis conseguirla para listar las distintas peliculas con el linked
+
+     */
 	@Override
 	public LinkedList<Pelicula> getListaPelis(){		
 		return listaPelis.getListaPelis();
   }
 	
-}
+}//cierre de la capa de servicios
